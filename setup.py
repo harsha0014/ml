a6af143b335b23e031267b896942230330c50b56
harsha0014 <kharsha2024@gmail.com>
2024-07-27 14:49:11 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+
+def get_requirements(file_path: str) -> List[str]:
+    '''
+    This function will return the list of components
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='harsha',
+    author_email='kharsha2024@gmail.com',
+    packages=find_packages(),  # Correctly call find_packages() with parentheses
+    install_requires=get_requirements('requirements.txt')
+)
